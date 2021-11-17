1dbe51d868c63f6cb63c78f64a7e64ed7ed5bf0c
EricGrunblatt <ericgrunblatt@gmail.com>
2021-11-17 12:19:17 -0500

    Initial commit

---
+++ client/src/components/index.js
@@ -0,0 +1,33 @@
+import AppBanner from './AppBanner'
+import AccountErrorModal from './AccountErrorModal'
+import DeleteModal from './DeleteModal'
+import EditToolbar from './EditToolbar'
+import HomeScreen from './HomeScreen'
+import HomeWrapper from './HomeWrapper'
+import ListCard from './ListCard'
+import RegisterScreen from './RegisterScreen'
+import SignIn from './SignIn'
+import SplashScreen from './SplashScreen'
+import Statusbar from './Statusbar'
+import Top5Item from './Top5Item'
+import WorkspaceScreen from './WorkspaceScreen'
+/*
+    This serves as a module so that we can import
+    all the other components as we wish.
+    
+    @author McKilla Gorilla
+*/
+export { 
+    AppBanner, 
+    AccountErrorModal,
+    DeleteModal,
+    EditToolbar, 
+    HomeScreen,
+    HomeWrapper,
+    ListCard, 
+    RegisterScreen,
+    SignIn,
+    SplashScreen,
+    Statusbar, 
+    Top5Item, 
+    WorkspaceScreen }
